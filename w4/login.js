be61ec075e61941ea36dc0bb47b6aaca2fa64470
Yiru <rosa60721@gmail.com>
2023-02-04 18:00:39 +0800

    w4 Lv2

---
+++ w4/login.js
@@ -0,0 +1,31 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+const url = 'https://vue3-course-api.hexschool.io/v2/';
+
+const app = {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login() {
+      axios.post(`${url}/admin/signin`,this.user)
+      .then((res) => {
+        alert("登入成功");
+        const { expired, token } = res.data;
+        //保存token、experid(存在cookie中，就不必一直重新登入)
+        document.cookie = `yiruToken=${ token }; expires=${ new Date(expired)};`;
+        window.location = 'productList_w4.html';
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      })
+    }
+  }
+}
+
+createApp(app).mount('#app')
