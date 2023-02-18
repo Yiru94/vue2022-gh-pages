2f53050ec9261cc2cb4f97d445fb4dd4dd365474
Yiru <rosa60721@gmail.com>
2023-02-18 15:17:59 +0800

    W5 Lv2

--- w5/cart.js
+++ w5/cart.js
@@ -1,8 +1,25 @@
-import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.esm-browser.min.js';
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.esm-browser.prod.min.js';
 
 const url = 'https://vue3-course-api.hexschool.io/v2/';
 const path = 'vuehwyiru';
 
+//加入驗證規則
+Object.keys(VeeValidateRules).forEach(rule => {
+  if (rule !== 'default') {
+    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+  }
+});
+
+// 讀取外部的資源
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
+
+//查看商品
 const productModal = {
   //當id變動時(用watch監聽)，去取得遠端資料，並呈現modal
   props: ['id', 'addToCart', 'openModal', 'loadingItem'],
@@ -24,7 +41,6 @@ const productModal = {
       if(this.id) {
         axios.get(`${url}api/${path}/product/${this.id}`)
           .then((res) => {
-            console.log('單一產品列表:', res.data.product);
             this.tempProduct = res.data.product;
             this.modal.show();
           })
@@ -42,24 +58,32 @@ const productModal = {
   }
 };
 
-const app = createApp({
+const app = Vue.createApp({
   data() {
     return {
       products: [],
       productId: "",
       cart: {},
       loadingItem: '', //存id，當此id存在就不能操作
-      //isLoading: true,
+      isLoading: false,
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+        },
+        message: ''
+      },
     }
   },
   methods:{
     getProducts() {
-      //this.isLoading = true;
+      this.isLoading = true;
       axios.get(`${url}api/${path}/products/all`)
         .then((res) => {
-          console.log('產品列表:', res.data.products);
           this.products = res.data.products;
-          //this.isLoading = false;
+          this.isLoading = false;
         })
         .catch((err) => {
           console.log(err.data.message);
@@ -76,7 +100,6 @@ const app = createApp({
       this.loadingItem = product_id;
       axios.post(`${url}api/${path}/cart`, { data })
         .then((res) => {
-          console.log('加入購物車:', res.data)
           this.$refs.productModal.closeModal();
           this.getCarts();
           this.loadingItem = '';
@@ -88,7 +111,6 @@ const app = createApp({
     getCarts() {
       axios.get(`${url}api/${path}/cart`)
         .then((res) => {
-          console.log('購物車列表:', res.data);
           this.cart = res.data.data;
         })
         .catch((err) => {
@@ -103,7 +125,6 @@ const app = createApp({
       this.loadingItem = item.id;//參照html的disabled功能
       axios.put(`${url}api/${path}/cart/${item.id}`, { data })
         .then((res) => {
-          console.log('修改購物車:', res.data)
           this.getCarts();
           this.loadingItem = '';
         })
@@ -115,7 +136,6 @@ const app = createApp({
       this.loadingItem = item.id;
       axios.delete(`${url}api/${path}/cart/${item.id}`)
         .then((res) => {
-          console.log('刪除購物車:', res.data)
           this.getCarts();
           this.loadingItem = '';
         })
@@ -126,12 +146,34 @@ const app = createApp({
     deleteCarts() {
       axios.delete(`${url}api/${path}/carts`)
         .then((res) => {
-          console.log('刪除全部購物車:', res.data)
           this.getCarts();
         })
         .catch((err) => {
           alert(err.data.message);
         });
+    },
+    //電話驗證
+    isPhone(value) {
+      if(!value) {
+        return '電話 為必填'
+      };
+      const phoneNumber = /^(09)[0-9]{8}$/
+      return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+    },
+    //送訂單
+    sendOrder() {
+      const order = this.form;
+      console.log(order);
+      axios.post(`${url}api/${path}/order`, { "data": order })
+        .then((res) => {
+          console.log(res)
+          alert(res.data.message);
+          this.$refs.form.resetForm();//淨空表單
+          this.getCarts();
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        })
     }
   },
   components:{
@@ -143,8 +185,12 @@ const app = createApp({
   }
 });
 
-console.log(VueLoading)
 //loading
 app.component('loading', VueLoading.Component);
+
+//VeeValidation
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
 app.mount('#app');
